2254ec0e08b17079d9a09153bd7136505c149b0e
mikedorin <78502248+mikedorin@users.noreply.github.com>
2024-02-15 13:40:26 -0600

    Update UserRepository.java
    add an extra space

--- sql-world-demo-1/src/main/java/edu/stthomas/sqlworlddemo1/UserRepository.java
+++ sql-world-demo-1/src/main/java/edu/stthomas/sqlworlddemo1/UserRepository.java
@@ -6,6 +6,7 @@ import edu.stthomas.sqlworlddemo1.User;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
+//
 
 public interface UserRepository extends CrudRepository<User, Integer> {
 
