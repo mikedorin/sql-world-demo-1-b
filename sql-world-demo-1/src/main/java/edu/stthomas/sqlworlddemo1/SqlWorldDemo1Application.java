63eb14a77fc459c0147857bf9906d1a90f097d07
mikedorin <78502248+mikedorin@users.noreply.github.com>
2024-02-15 13:37:59 -0600

    inital checkin
    initial checkin

---
+++ sql-world-demo-1/src/main/java/edu/stthomas/sqlworlddemo1/SqlWorldDemo1Application.java
@@ -0,0 +1,13 @@
+package edu.stthomas.sqlworlddemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlWorldDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlWorldDemo1Application.class, args);
+	}
+
+}
---
+++ sql-world-demo-1/src/test/java/edu/stthomas/sqlworlddemo1/SqlWorldDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.stthomas.sqlworlddemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlWorldDemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
